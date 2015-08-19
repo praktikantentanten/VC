c5ff2213378020c30c864a65403460b233694df2
Michael Dandan <michaeldandan@hotmail.de>
2015-08-19 03:45:27 +0200

    Code Generell GEIL gemacht. es ist 3:40 du pisser. "du machst nie was" FUUUUUUUUU.

--- ProjektX/ProjektX/Quelle1.cpp
+++ ProjektX/ProjektX/Quelle1.cpp
@@ -1,55 +1,62 @@
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2\highgui\highgui.hpp>
 #include <opencv2\features2d\features2d.hpp>
 #include <opencv2\features2d.hpp>
 #include <opencv2\imgproc\imgproc.hpp>
 #include <iostream>
-#include <chrono>
 #include <ctime>
 
-using namespace std;
-using namespace cv;
+using namespace std;	// Erleichterung durch Weglassen von Header std
+using namespace cv;	// Erleichterung durch Weglassen von Header cv
 
 int main(int argc, char** argv)
 {
-	std::cout << "OpenCV version: "
+	std::cout << "OpenCV Version: " // Ausgabe der Version von OpenCV
 		<< CV_MAJOR_VERSION << "."
 		<< CV_MINOR_VERSION << "."
 		<< CV_SUBMINOR_VERSION
 		<< std::endl;
-	Mat im = imread("adam1.png", 0);
-	if (im.empty())
+
+	Mat img = imread("adam1.png"); // Eingelesenes Originalbild
+
+	if (img.empty())	//Abfrage ob eingelesenes Bild leer ist
 	{
-		cout << "Cannot open image!" << endl;
+		cout << "Kann Bild nicht öffnen!" << endl; // Fehlermeldung bei nicht einlesbarem Bild
 		return -1;
 	}
-	Mat gray;
-	cvtColor(im, gray, cv::COLOR_BGR2GRAY);
-	int mnArea = 40 * 40;
-	int mxArea = im.rows*im.cols*0.4;
-	vector< vector< Point > > ptblobs;
-	vector<Rect> bboxes;
-	chrono::time_point<chrono::system_clock> start, end;
 
-	start = chrono::system_clock::now();
+	
 
-	Ptr<cv::MSER> mser = cv::MSER::create(1, mnArea, mxArea, 0.25, 0.2);
+	Mat gray; // Graubild des Eingelesenen Bildes
+	cvtColor(img, gray, cv::COLOR_BGR2GRAY);  // Konvertierung von eingelesenem Bild in Graubild
+
+	int minArea = 60;	// MSER Eingabevariable: minimaler Bereich
+	int maxArea = 14400;	// MSER Eingabevariable: maximaler Bereich
+	int delta = 5;	// MSER Eingabevariable: 
+	double variation = 0.25;	// MSER Eingabevariable:
+	double diversity = 0.2;	// MSER Eingabevariable:
+	int maxEvo = 200;	// MSER Eingabevariable:
+	double arTh = 1.01;	// MSER Eingabevariable:
+	double minMar = 0.003;	// MSER Eingabevariable:
+	int ebs = 5;
+	vector< vector< Point > > ptblobs; //Speicherortes für die Punkte der gefundenen Blobs 
+		//"a blob is a region of an image in which some properties are constant or approximately constant"
+	vector<Rect> bboxes;	// Speicherort für Rechtecke
+
+
+	static Ptr<MSER> mser = MSER::create(delta, minArea, maxArea, variation, diversity, maxEvo, arTh, minMar, ebs); //  MSER- Detektor
 	mser->detectRegions(gray, ptblobs, bboxes);
+
+	Mat img_regions;	// Bild mit markierten MSER Regionen
 	
-	MserFeatureDetector mser(1, mnArea, mxArea, 0.25, 0.2);
-	mser(gray, ptblobs); 
+	//ellipse()
 
-	
-	end = std::chrono::system_clock::now();
+		cout << "MSER Berechnung abgeschlossen" << endl;
 
-	chrono::duration<double> elapsed_seconds = end - start;
-	time_t end_time = chrono::system_clock::to_time_t(end);
+		namedWindow("Originalbild");
+		imshow("Originalbild", img);
+		//namedWindow("MSER");
+		//imshow("MSER", img_regions);
+		waitKey(0);
 
-	cout << "finished computation at " << ctime(&end_time)
-		<< "elapsed time: " << elapsed_seconds.count() << "s\n";
-
-	namedWindow("image", cv::WINDOW_NORMAL);
-	imshow("image", im);
-	waitKey(0);
-
-	return 0;
-}
+		return 0;
+	}
