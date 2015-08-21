3c4869271f353f51480d63e3914a5de4f9c4e5c9
PraktikumVC <mens90@web.de>
2015-08-21 15:08:21 +0200

    Vollständiger Upload

---
+++ ProjektX/ProjektX/BildSchneiden.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include "BildSchneiden.h"
+
+
+BildSchneiden::BildSchneiden()
+{
+}
+
+
+BildSchneiden::~BildSchneiden()
+{
+}
