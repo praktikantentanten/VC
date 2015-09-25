586aca5cd7dcaa8ad6690b3fb8f2c54ba16018a7
PraktikumVC <mens90@web.de>
2015-09-25 23:24:42 +0200

    speichern funktioniert

--- ProjektX/ProjektX/Speicher.h
+++ ProjektX/ProjektX/Speicher.h
@@ -13,15 +13,7 @@ public:
 	~Speicher();
 	bool Speicher::Save(cv::Mat img1, cv::Mat img2, std::string ordner, std::string uordner);
 	bool SetFolder(std::string ordner);
-	DWORD AddAceToObjectsSecurityDescriptor(
-		LPTSTR pszObjName,          // name of object
-		SE_OBJECT_TYPE ObjectType,  // type of object
-		LPTSTR pszTrustee,          // trustee for new ACE
-		TRUSTEE_FORM TrusteeForm,   // format of trustee structure
-		DWORD dwAccessRights,       // access mask for new ACE
-		ACCESS_MODE AccessMode = GRANT_ACCES,     // type of ACE
-		DWORD dwInheritance         // inheritance flags for new ACE
-		)
+
 	//LPSTR curDirectory;
 };
 
