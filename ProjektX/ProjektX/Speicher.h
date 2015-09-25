0b256d29c73edcdad2fac501a7914680cd24269a
PraktikumVC <mens90@web.de>
2015-09-25 11:28:12 +0200

    Klassendiagramm ergänzt, Schreibrechte probiert

--- ProjektX/ProjektX/Speicher.h
+++ ProjektX/ProjektX/Speicher.h
@@ -4,6 +4,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <stdio.h>
 
 class Speicher
 {
@@ -12,6 +13,15 @@ public:
 	~Speicher();
 	bool Speicher::Save(cv::Mat img1, cv::Mat img2, std::string ordner, std::string uordner);
 	bool SetFolder(std::string ordner);
+	DWORD AddAceToObjectsSecurityDescriptor(
+		LPTSTR pszObjName,          // name of object
+		SE_OBJECT_TYPE ObjectType,  // type of object
+		LPTSTR pszTrustee,          // trustee for new ACE
+		TRUSTEE_FORM TrusteeForm,   // format of trustee structure
+		DWORD dwAccessRights,       // access mask for new ACE
+		ACCESS_MODE AccessMode = GRANT_ACCES,     // type of ACE
+		DWORD dwInheritance         // inheritance flags for new ACE
+		)
 	//LPSTR curDirectory;
 };
 
